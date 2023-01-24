1f428cb172535b5922d06eb393331ec2fd696c5b
changzhibin <758589594@qq.com>
2023-01-24 17:01:27 +0800

    version 2.0

--- src/main/java/com/nuc/project/servlet/module/AddGoodsServlet.java
+++ src/main/java/com/nuc/project/servlet/module/AddGoodsServlet.java
@@ -23,6 +23,8 @@ public class AddGoodsServlet extends ViewBaseServlet {
         // 声明要访问的首页的逻辑视图
         String templateName = "add-goods";
 
+        String name = "jfslfj";
+
         // 调用父类的方法根据逻辑视图名称渲染视图
         processTemplate(templateName, req, resp);
     }
